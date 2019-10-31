2d1eee3600b8edd4257f26be0c23f2b2dd158507
rayegg111 <55218513+rayegg111@users.noreply.github.com>
2019-10-31 10:23:54 +0800

    3

---
+++ MotionCapture/Assets/Scripts/lady.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class lady : MonoBehaviour
+{
+    // 修飾詞 傳回類型 方法名稱 (參數) { 敘述 }
+    // void 為無傳回類型
+    /// <summary>
+    /// 走路
+    /// </summary>
+    void Walk()
+    {
+
+    }
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    void Jump()
+    {
+
+    }
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    void Attack()
+    {
+
+    }
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    void Hurt()
+    {
+
+    }
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    void Die()
+    {
+
+    }
+}
