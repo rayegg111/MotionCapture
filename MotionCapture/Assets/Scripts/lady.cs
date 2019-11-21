5569feb27115f76571f13ae2bf7a1bc14b1c7f9b
rayegg111 <55218513+rayegg111@users.noreply.github.com>
2019-11-21 10:25:25 +0800

    1121

--- MotionCapture/Assets/Scripts/lady.cs
+++ MotionCapture/Assets/Scripts/lady.cs
@@ -7,13 +7,15 @@ public class lady : MonoBehaviour
 
     [Header("速度"), Range(0f, 50f)]
     public float speed = 10f;
+    [Header("旋轉"), Range(0f, 100f)]
+    public float rotate = 5f;
 
     [Header("動畫控制器")]
     public string aniWalk = "走路";
     public string aniJump = "跳躍";
-    public string aniAtk =  "攻擊";
+    public string aniAtk  = "攻擊";
     public string aniHurt = "受傷";
-    public string aniDie =  "死亡";
+    public string aniDie  = "死亡";
     
 
 
@@ -25,14 +27,14 @@ public class lady : MonoBehaviour
 
     private void Update()
     {
-        
+        Attack();
+        Rotate();
     }
 
     private void FixedUpdate()
     {
         Walk();
         Jump();
-        Attack();
     }
 
     // 修飾詞 傳回類型 方法名稱 (參數) { 敘述 }
@@ -43,8 +45,24 @@ public class lady : MonoBehaviour
     void Walk()
     {
         ani.SetBool(aniWalk, Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0);
-        rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed);
+        //rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed);
+
+        //transform.forward (0,0,1)
+        //transform.right   (1,0,0)
+        //transform.up      (0,1,0)
+        rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed + transform.right * Input.GetAxisRaw("Horizontal") * speed);
     }
+
+    /// <summary>
+    /// 旋轉
+    /// </summary>
+    private void  Rotate()
+    {
+        float x = Input.GetAxis("Mouse X");  // 滑鼠左右，左 -1、右 1
+        //Time.deltatime 一幀的時間
+        transform.Rotate(0, x * rotate * Time.deltaTime, 0);
+    }
+    
     /// <summary>
     /// 跳躍
     /// </summary>
