b9adf14f71ffb9976cf491f2351b1f5c289e13a2
rayegg111 <55218513+rayegg111@users.noreply.github.com>
2019-12-05 10:46:25 +0800

    1

--- MotionCapture/Assets/Scripts/lady.cs
+++ MotionCapture/Assets/Scripts/lady.cs
@@ -9,6 +9,8 @@ public class lady : MonoBehaviour
     public float speed = 10f;
     [Header("旋轉"), Range(0f, 100f)]
     public float rotate = 5f;
+    [Header("血量"), Range(100, 500)]
+    public int hp = 100;
 
     [Header("動畫控制器")]
     public string aniWalk = "走路";
@@ -16,8 +18,25 @@ public class lady : MonoBehaviour
     public string aniAtk  = "攻擊";
     public string aniHurt = "受傷";
     public string aniDie  = "死亡";
-    
 
+    // 屬性 可以設定權限 取得 get、設定 set
+    // 修飾詞 類型 名稱 { 取得 設定 }
+    public int MyProperty { get; set; }
+
+    public bool isAttack
+    {
+        get
+        {
+            return ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊"); //傳回
+        }
+    }
+    public bool isHurt
+    {
+        get
+        {
+            return ani.GetCurrentAnimatorStateInfo(0).IsName("受傷"); //傳回
+        }
+    }
 
     private void Start()
     {
@@ -27,14 +46,31 @@ public class lady : MonoBehaviour
 
     private void Update()
     {
+        print("攻擊：" + isAttack);
+        print("受傷：" + isHurt);
+        if (isAttack || isHurt) return; //跳出
+
         Attack();
         Rotate();
     }
 
     private void FixedUpdate()
     {
+        if (isAttack || isHurt) return; //跳出
+
         Walk();
         Jump();
+    }
+
+    // 觸發事件：碰到勾選 IsTrigger 碰撞器開始時候執行一次
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.tag);
+
+        if (other.tag == "陷阱")
+        {
+            Hurt();
+        }
     }
 
     // 修飾詞 傳回類型 方法名稱 (參數) { 敘述 }
@@ -89,6 +125,8 @@ public class lady : MonoBehaviour
     void Hurt()
     {
         ani.SetTrigger(aniHurt);
+        hp -= 20;
+        if (hp <= 0) Die();
     }
     /// <summary>
     /// 死亡
@@ -96,5 +134,9 @@ public class lady : MonoBehaviour
     void Die()
     {
         ani.SetBool(aniDie, true);
+
+        //this 此腳本
+        //enabled 啟動
+        this.enabled = false;
     }
 }
